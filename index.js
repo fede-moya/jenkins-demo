6fba6e320d9758e3f784763464a3455103581d50
Federico <federicomoyamartin@gmail.com>
2019-09-24 17:21:11 -0300

    Add extremely simple node app with a test

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require('express');
+ 
+let app = express();
+
+app.get('/', function (req, res) {
+ res.send('hello world');
+});
+
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+let request = require('supertest');
+let app = require('../index.js');
+describe('GET /', function() {
+ it('respond with hello world', function(done) {
+ //navigate to root and check the the response is "hello world"
+ request(app).get('/').expect('hello world', done);
+ });
+});
